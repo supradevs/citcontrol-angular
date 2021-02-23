0c7ca064e2a72cda4b844b68494d279cee0fd9d9
sam <sambenzt@gmail.com>
2021-02-22 22:26:01 -0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './template/login/login.component';
-import { SchedulerComponent } from './template/scheduler/scheduler.component';
 
 
 const routes: Routes = [
-  {
-    path:'template/login',
-    component: LoginComponent
-  },
-  {
-    path:'template/scheduler',
-    component: SchedulerComponent
-  }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './template/login/login.component';
-import { RequestsComponent } from './template/requests/requests.component';
-import { SchedulerComponent } from './template/scheduler/scheduler.component';
-import { CalendarComponent } from './template/calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RequestsComponent,
-    SchedulerComponent,
-    CalendarComponent
   ],
   imports: [
     BrowserModule,
